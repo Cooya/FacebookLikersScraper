68f286178c090497f7d820af35abbfcd19323ab2
Nicolas <nicolas.marcy@etu.u-bordeaux.fr>
2017-12-18 17:49:54 +0100

    csv file generator

---
+++ collectionToCsv.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const json2csv = require('json2csv');
+const MongoClient = require('mongodb').MongoClient;
+
+(async () => {
+	const config = require('./config.json');
+
+	const collection = await getDatabaseCollection(config.databaseUrl, config.collectionName);
+	console.log('Collection "' + config.collectionName + '" selected.');
+
+	const profiles = await collection.find({page: config.pageName}).toArray();
+	console.log('Profiles from page = "' + config.pageName + '" retrieved.');
+
+	try {
+		const result = json2csv({data: profiles, fields: config.csvFields});
+		fs.writeFile(config.pageName + '.csv', result, (err) => {
+			if(err)
+				console.error(err);
+			else
+				console.log('CSV file "' + config.pageName + '.csv" generated successfully.');
+			process.exit(0);
+		});
+	} catch(err) {
+		console.error(err);
+	}
+})();
+
+function getDatabaseCollection(databaseUrl, collectionName) {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(databaseUrl, (err, db) => {
+			if(err)
+				reject(err);
+			else {
+				db.collection(collectionName, (err, coll) => {
+					if(err)
+						reject(err);
+					else
+						resolve(coll);
+				});
+			}
+		});
+	});
+}
